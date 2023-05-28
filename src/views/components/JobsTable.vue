ec206ae4ccf434ef0a4b4c5ed856ce5b5db0b79f
jamespurnama1 <james.henry.purnama0@gmail.com>
2023-05-28 12:26:03 +0700

    limit url with ellipses

--- src/views/components/JobsTable.vue
+++ src/views/components/JobsTable.vue
@@ -61,7 +61,7 @@
                 <span class="badge badge-sm" :class="badgeClasses(job.type)">{{ job.type.replace(/([A-Z])/g, ' $1').trim() }}</span>
               </td>
               <td class="align-middle text-center">
-                <a :href="job.link ? job.link : ''" target="_blank" rel="noreferrer noopener" class="text-secondary text-xs font-weight-bold">{{ job.link ? job.link : '' }}</a>
+                <a :href="job.link ? job.link : ''" target="_blank" rel="noreferrer noopener" class="text-secondary limit text-xs font-weight-bold">{{ job.link ? job.link : '' }}</a>
               </td>
               <td class="align-middle text-center">
                 <span class="text-secondary text-xs font-weight-bold">{{ job.date ? formatDate(job.date) : '' }}</span>
